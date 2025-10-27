24ff3f848c1ae4a5ec2e41cb7f111f4a59ddb211
Matthias Bünger <hardcore_army@web.de>
2025-10-27 17:05:09 +0100

    Spotless

--- src/test/java/org/apache/maven/plugins/help/DescribeMojoTest.java
+++ src/test/java/org/apache/maven/plugins/help/DescribeMojoTest.java
@@ -38,6 +38,7 @@ import org.apache.maven.project.MavenProject;
 import org.eclipse.aether.RepositorySystemSession;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
@@ -48,7 +49,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
 
 /**
  * @author <a href="mailto:vincent.siveton@gmail.com">Vincent Siveton</a>
