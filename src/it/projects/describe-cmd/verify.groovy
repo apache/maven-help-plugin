bc9402faf1c9d9d8175fb7045c6a4ffe4966542a
Tamas Cservenak <tamas@cservenak.net>
2026-06-09 23:13:53 +0200

    Fix issue 367

--- src/it/projects/describe-cmd/verify.groovy
+++ src/it/projects/describe-cmd/verify.groovy
@@ -22,7 +22,7 @@ def result = new File(basedir, 'result-deploy.txt').text;
 def ls = System.getProperty( "line.separator" );
 
 // used deprecated methods - FIXME in DescribeMojo
-if (mavenVersion.startsWith('4.') || mavenVersion.startsWith('3.10.')) {
+if (mavenVersion.startsWith('4.')) {
     assert result.contains("'deploy' is a phase within the 'default' lifecycle, which has the following phases:")
 } else {
     assert result.contains("'deploy' is a phase corresponding to this plugin:" + ls +
