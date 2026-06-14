apache/maven-help-plugin

commit 2be321113d1dfedc174234c8bafd23ee7e6230fd

Update Parent to 45 and invoker plugin 3.9.1 (Java 25) (#344)
* Update Parent to 45 and invoker plugin 3.9.1 (Java 25)

* Spotless

* Try to fix ITs

---------

Co-authored-by: Slawomir Jaranowski <s.jaranowski@gmail.com>

src/it/projects/active-profiles_multimodule/verify.groovy
@@ -20,7 +20,15 @@
 def buildLog = new File( basedir, 'build.log' ); 
 assert buildLog.exists()
 
-assert 2 == buildLog.text.count( ' - mph79 (source: org.apache.maven.its.help:active-profiles_multimodule:1.0-SNAPSHOT)' )
-assert 1 == buildLog.text.count( ' - mph79-child (source: org.apache.maven.its.help:active-profiles_multimodule-module:1.0-SNAPSHOT)' )
-assert 0 == buildLog.text.count( ' - mph79-child-inactive' )
-assert 2 == buildLog.text.count( ' - it-repo (source: external)' )
+if (mavenVersion.startsWith( '4.' )) {
+    assert 1 == buildLog.text.count(' - mph79 (source: org.apache.maven.its.help:active-profiles_multimodule:pom:1.0-SNAPSHOT)')
+    assert 1 == buildLog.text.count(' - mph79 (source: org.apache.maven.its.help:active-profiles_multimodule-module:jar:1.0-SNAPSHOT)')
+    assert 1 == buildLog.text.count('- mph79-child (source: org.apache.maven.its.help:active-profiles_multimodule-module:jar:1.0-SNAPSHOT)')
+
+} else {
+    assert 2 == buildLog.text.count(' - mph79 (source: org.apache.maven.its.help:active-profiles_multimodule:1.0-SNAPSHOT)')
+    assert 1 == buildLog.text.count(' - mph79-child (source: org.apache.maven.its.help:active-profiles_multimodule-module:1.0-SNAPSHOT)')
+}
+
+assert 0 == buildLog.text.count(' - mph79-child-inactive')
+assert 2 == buildLog.text.count(' - it-repo (source: external)')

src/it/projects/effective-pom_properties/verify.groovy
@@ -19,11 +19,25 @@
 def buildLog = new File(basedir, 'build.log'); 
 assert buildLog.exists()
 
-def LS = System.getProperty("line.separator") 
-assert buildLog.text.find(
-'(?s)' +
-'  <properties>' + LS +
-'    <maven\\.compiler\\.source>1\\.6</maven\\.compiler\\.source>' + LS +
-'    <maven\\.compiler\\.target>1\\.6</maven\\.compiler\\.target>' + LS +
-'.*' +
-'  </properties>') != null
+def LS = System.getProperty("line.separator")
+if (mavenVersion.startsWith('4.')) {
+    // we have defined properties maven.compiler.source/target to 8 as user properties for execution
+    // Maven 4 take precedence over the pom properties
+    // also order of properties is changed in Maven 4
+    assert buildLog.text.find(
+            '(?s)' +
+                    '  <properties>' + LS +
+                    '    <maven\\.compiler\\.target>8</maven\\.compiler\\.target>' + LS +
+                    '    <maven\\.compiler\\.source>8</maven\\.compiler\\.source>' + LS +
+                    '.*' +
+                    '  </properties>') != null
+
+} else {
+    assert buildLog.text.find(
+            '(?s)' +
+                    '  <properties>' + LS +
+                    '    <maven\\.compiler\\.source>1\\.6</maven\\.compiler\\.source>' + LS +
+                    '    <maven\\.compiler\\.target>1\\.6</maven\\.compiler\\.target>' + LS +
+                    '.*' +
+                    '  </properties>') != null
+}

src/it/projects/describe-cmd/verify.groovy
@@ -21,8 +21,12 @@ def result = new File(basedir, 'result-deploy.txt').text;
 
 def ls = System.getProperty( "line.separator" );
 
-assert result.contains("'deploy' is a phase corresponding to this plugin:" + ls +
-                       "org.apache.maven.plugins:maven-deploy-plugin")
+if (mavenVersion.startsWith('4.')) {
+    assert result.contains("'deploy' is a phase within the 'default' lifecycle, which has the following phases:")
+} else {
+    assert result.contains("'deploy' is a phase corresponding to this plugin:" + ls +
+            "org.apache.maven.plugins:maven-deploy-plugin")
+}
 
 result = new File(basedir, 'result-site.txt').text;
 

pom.xml
@@ -23,7 +23,7 @@
   <parent>
     <groupId>org.apache.maven.plugins</groupId>
     <artifactId>maven-plugins</artifactId>
-    <version>43</version>
+    <version>45</version>
     <relativePath />
   </parent>
 
@@ -166,6 +166,7 @@
     <dependency>
       <groupId>org.apache.maven.plugin-tools</groupId>
       <artifactId>maven-plugin-annotations</artifactId>
+      <version>${version.maven-plugin-tools}</version>
       <scope>provided</scope>
     </dependency>
 
@@ -276,6 +277,7 @@
           <plugin>
             <groupId>org.apache.maven.plugins</groupId>
             <artifactId>maven-invoker-plugin</artifactId>
+            <version>3.9.1</version>
             <configuration>
               <localRepositoryPath>${project.build.directory}/local-repo</localRepositoryPath>
               <projectsDirectory>src/it/projects</projectsDirectory>

src/it/projects/evaluate-artifact-with-expression-with-output/pom.xml
@@ -47,7 +47,7 @@ under the License.
       <plugin>
         <groupId>org.apache.maven.plugins</groupId>
         <artifactId>maven-enforcer-plugin</artifactId>
-        <version>1.4.1</version>
+        <version>@version.maven-enforcer-plugin@</version>
         <executions>
           <execution>
             <id>enforce-currentproject</id>

src/it/projects/evaluate-forceStdout-raw-streems/invoker.properties
@@ -15,8 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-invoker.goals.1 = ${project.groupId}:${project.artifactId}:${project.version}:describe
-invoker.goals.2 = ${project.groupId}:${project.artifactId}:${project.version}:describe
-
-invoker.userPropertiesFile.1 = test-deploy.properties
-invoker.userPropertiesFile.2 = test-site.properties
+invoker.goals = ${project.groupId}:${project.artifactId}:${project.version}:evaluate -q --raw-streams
+invoker.debug = false
+# --raw-streams available since Maven 3.9.11
+invoker.maven.version = 3.9.11+

src/it/projects/evaluate-forceStdout-raw-streems/pom.xml
@@ -1,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<!--
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+-->
+
+<project>
+  <modelVersion>4.0.0</modelVersion>
+
+  <groupId>org.apache.maven.its.help</groupId>
+  <artifactId>test</artifactId>
+  <version>1.0</version>
+
+  <description>
+    The description.
+  </description>
+
+  <build>
+    <plugins>
+    </plugins>
+  </build>
+</project>

commit edfd07af6d888d9b0b4c499007f7b995d4b7fefd

revert broken IT updates from #344

src/it/projects/active-profiles_multimodule/verify.groovy
@@ -20,15 +20,7 @@
 def buildLog = new File( basedir, 'build.log' ); 
 assert buildLog.exists()
 
-if (mavenVersion.startsWith( '4.' )) {
-    assert 1 == buildLog.text.count(' - mph79 (source: org.apache.maven.its.help:active-profiles_multimodule:pom:1.0-SNAPSHOT)')
-    assert 1 == buildLog.text.count(' - mph79 (source: org.apache.maven.its.help:active-profiles_multimodule-module:jar:1.0-SNAPSHOT)')
-    assert 1 == buildLog.text.count('- mph79-child (source: org.apache.maven.its.help:active-profiles_multimodule-module:jar:1.0-SNAPSHOT)')
-
-} else {
-    assert 2 == buildLog.text.count(' - mph79 (source: org.apache.maven.its.help:active-profiles_multimodule:1.0-SNAPSHOT)')
-    assert 1 == buildLog.text.count(' - mph79-child (source: org.apache.maven.its.help:active-profiles_multimodule-module:1.0-SNAPSHOT)')
-}
-
-assert 0 == buildLog.text.count(' - mph79-child-inactive')
-assert 2 == buildLog.text.count(' - it-repo (source: external)')
+assert 2 == buildLog.text.count( ' - mph79 (source: org.apache.maven.its.help:active-profiles_multimodule:1.0-SNAPSHOT)' )
+assert 1 == buildLog.text.count( ' - mph79-child (source: org.apache.maven.its.help:active-profiles_multimodule-module:1.0-SNAPSHOT)' )
+assert 0 == buildLog.text.count( ' - mph79-child-inactive' )
+assert 2 == buildLog.text.count( ' - it-repo (source: external)' )

src/it/projects/effective-pom_properties/verify.groovy
@@ -16,22 +16,19 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-def buildLog = new File(basedir, 'build.log'); 
+def buildLog = new File(basedir, 'build.log');
 assert buildLog.exists()
 
 def LS = System.getProperty("line.separator")
 if (mavenVersion.startsWith('4.')) {
-    // we have defined properties maven.compiler.source/target to 8 as user properties for execution
-    // Maven 4 take precedence over the pom properties
-    // also order of properties is changed in Maven 4
+    // with Maven 4, order of properties is reversed from original pom.xml
     assert buildLog.text.find(
             '(?s)' +
                     '  <properties>' + LS +
-                    '    <maven\\.compiler\\.target>8</maven\\.compiler\\.target>' + LS +
-                    '    <maven\\.compiler\\.source>8</maven\\.compiler\\.source>' + LS +
+                    '    <maven\\.compiler\\.target>1\\.6</maven\\.compiler\\.target>' + LS +
+                    '    <maven\\.compiler\\.source>1\\.6</maven\\.compiler\\.source>' + LS +
                     '.*' +
                     '  </properties>') != null
-
 } else {
     assert buildLog.text.find(
             '(?s)' +