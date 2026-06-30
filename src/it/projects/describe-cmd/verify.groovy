9be59d03cdebd772a0d441628522ce7cd276ea91
Tamas Cservenak <tamas@cservenak.net>
2026-06-30 14:24:29 +0200

    Enable Maven 4
    For this to work, several small changes and 2 IT needs to
    be prevented to run on Maven 4.

--- src/it/projects/describe-cmd/verify.groovy
+++ src/it/projects/describe-cmd/verify.groovy
@@ -21,13 +21,8 @@ def result = new File(basedir, 'result-deploy.txt').text;
 
 def ls = System.getProperty( "line.separator" );
 
-// used deprecated methods - FIXME in DescribeMojo
-if (mavenVersion.startsWith('4.')) {
-    assert result.contains("'deploy' is a phase within the 'default' lifecycle, which has the following phases:")
-} else {
-    assert result.contains("'deploy' is a phase corresponding to this plugin:" + ls +
-            "org.apache.maven.plugins:maven-deploy-plugin")
-}
+assert result.contains("'deploy' is a phase corresponding to this plugin:" + ls +
+        "org.apache.maven.plugins:maven-deploy-plugin")
 
 result = new File(basedir, 'result-site.txt').text;
 
